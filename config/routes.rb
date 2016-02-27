b08d8d40b30f60db528e540ea1018ad45024b598
LightbulbProductions <gerbermichelle23@gmail.com>
2016-02-27 13:42:57 +0000

    first addition

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Tumblful::Application.routes.draw do
   devise_for :users
+  resources :image_links
+  resources :text_posts
+  
+  resources :likes, :only => [:create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
